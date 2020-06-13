8c96e862f991e4c37b0a37742050af7510ad6f49
s910702s910702 <s910702s910702@gmail.com>
2020-06-13 23:44:46 +0800

    update hasher

--- backend/hasher.py
+++ backend/hasher.py
@@ -2,10 +2,26 @@ import hashlib
 import os
 
 
+def make_new_hash(password):
+	"""
+		input: password
+		return: salt, key
 
-salt = os.urandom(32) # Remember this
-print(salt)
-password = 'password123'
+	"""
 
-key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-print(key)
+	salt = os.urandom(32)
+	key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+
+	return salt, key
+
+
+def make_hash(salt, password):
+
+	"""
+		input: salt, password
+		return key
+
+	"""
+	key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)	
+
+	return key
